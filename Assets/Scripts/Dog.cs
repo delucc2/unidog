1d1368251e385eb54d969c556ed8aeae071245d2
Unknown <castrj5@rpi.edu>
2019-11-22 17:01:44 -0500

    cloc

--- Assets/Scripts/Dog.cs
+++ Assets/Scripts/Dog.cs
@@ -4,5 +4,9 @@ using UnityEngine;
 
 public class Dog : MonoBehaviour
 {
-
+    public int hunger = 0;
+    public int loneliness = 0;
+    public int fatigue = 0;
+    public int contenetment = 0;
+    public int bathroom = 0;
 }
--- Assets/Scripts/GameTime.cs
+++ Assets/Scripts/GameTime.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameTime : MonoBehaviour
 {
     int hours;
     int minutes;
+    string currTime;
+    string AMPM = "AM";
+    bool am = true;
+    public Text CanvasArea; 
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -37,6 +43,9 @@ public class GameTime : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        System.TimeSpan current = System.TimeSpan.FromMinutes(hours * 60 + minutes);
+        currTime = current.ToString();
+        currTime = currTime.Remove(5);
+        CanvasArea.text = string.Format("{0} {1} ", currTime, AMPM);
     }
 }
