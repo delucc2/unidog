2252a7fbf7bf5efaac672db4d890fe97122511b6
Unknown <castrj5@rpi.edu>
2019-11-22 16:34:58 -0500

    all inputs

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -19,11 +19,44 @@ public class PlayerInput : MonoBehaviour
             {
                 switch (a)
                 {
+                    case 'f':
+                        Debug.Log("Put food in the dog bowl");
+                        return;
+                    case 't':
+                        Debug.Log("Gave the dog a treat");
+                        return;
+                    case 'k':
+                        Debug.Log("Threw the stick");
+                        return;
+                    case 'p':
+                        Debug.Log("Pet the dog");
+                        return;
+                    case 'b':
+                        Debug.Log("Rubbed the dog's belly");
+                        return;
+                    case 'w':
+                        Debug.Log("Took the dog for a walk");
+                        return;
+                    case 'l':
+                        Debug.Log("Left the dog alone");
+                        return;
+                    case 'g':
+                        Debug.Log("Went to work");
+                        return;
+                    case 'a':
+                        Debug.Log("Return home from work");
+                        return;
                     case 'i':
-                        Debug.Log("Advanced fifteen minutes");
+                        Debug.Log("Wait fifteen minutes");
                         return;
                     case 'h':
-                        Debug.Log("Advanced one hour");
+                        Debug.Log("Wait one hour");
+                        return;
+                    case 'd':
+                        Debug.Log("Wait one day");
+                        return;
+                    case 's':
+                        Debug.Log("Played a strange sound");
                         return;
                     default:
                         return;
