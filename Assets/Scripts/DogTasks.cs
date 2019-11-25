7f60a5dd4d0f4d6886037949e57db335d98364c0
chengj6 <chengj6@rpi.edu>
2019-11-25 18:42:54 -0500

    something

--- Assets/Scripts/DogTasks.cs
+++ Assets/Scripts/DogTasks.cs
@@ -10,6 +10,44 @@ public class DogTasks : MonoBehaviour
         print("Doug pauses and looks towards where the noise came from, extremely tense.");
     }
 
+    //Positive Reactions
+    void EatFood()
+    {
+        print("Doug walks to his food bowl and eats.");
+    }
+
+    void EatTreat()
+    {
+        print("Doug eats the treat from your hands.");
+    }
+
+    void FetchStick()
+    {
+        print("Doug runs after the stick. He brings it back in his mouth.");
+    }
+
+    void Pet()
+    {
+        print("Doug wags his tail.");
+    }
+
+    void Belly()
+    {
+        print("Doug wags his tail and smiles with his mouth open, laying face up.");
+    }
+
+    void Walk()
+    {
+        print("Doug runs back into the house.");
+    }
+
+    void home()
+    {
+        print("Doug tackles you as you open the door, licking your face and running around you.");
+    }
+
+
+
     // Start is called before the first frame update
     void Start()
     {
